320500046fa068efe3bbfdae31763c9316a42375
Yosuke Nishimura <godblessyou317@gmail.com>
2013-08-15 05:30:04 +0900

    second

---
+++ lib/match_inchi/match_files.rb
@@ -0,0 +1,33 @@
+
+module MatchInChI
+	attr_reader :f1, :f2, :results
+	class Match_files
+		def initialize(f1, f2)
+			@f1 = f1 # InChI_file object of DB1
+			@f2 = f2 # InChI_file object of DB2
+			@results = Hash.new
+		end
+		def match
+			@f1.formulae2ids.each{ |formula, ids1|
+				ids2 = @f2.formulae2ids[formula]
+				next unless ids2
+				ids1.each{ |id1|
+					i1 = @f1.ids2inchi[id1]
+					ids2.each{ |id2|
+						i2 = @f2.ids2inchi[id2]
+						m = Match_InChI.new(i1, i2)
+						result = m.do_match
+						@results[[id1,id2]*","] = result if result
+					}
+				}
+			}
+		end
+		def make_output
+			@results.keys.sort.each{ |k|
+				next unless @results[k]
+				flag, hash = @results[k]
+				puts [k, flag, hash.to_inchi]*"\t"
+			}
+		end
+	end
+end
