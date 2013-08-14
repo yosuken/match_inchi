c85e7854f8606145941ea8f211a988bb5107cf31
Yosuke Nishimura <godblessyou317@gmail.com>
2013-08-15 06:03:18 +0900

    misc

--- bin/match_inchi.rb
+++ bin/match_inchi.rb
@@ -5,9 +5,14 @@ include MatchInChI
 
 f1, f2, f3 = ARGV
 unless f3
-	raise "usage: match_inchi input1 input2 output"
+	puts "usage: match_inchi input1 input2 output"
+	raise
 end
+$stderr.puts "input file1:#{f1}"
+$stderr.puts "input file2:#{f2}"
+$stderr.puts "output file:#{f3}"
 i1, i2 = [f1, f2].map{ |f| InChI_file.new(f) }
 fout = open(f3, "w")
 mf = Match_files.new(i1, i2)
+mf.match
 fout.puts mf.make_output
