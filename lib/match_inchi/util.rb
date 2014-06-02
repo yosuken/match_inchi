e99bebccbd68ec55041711abb85e644a6e0f9a73
Yosuke Nishimura <godblessyou317@gmail.com>
2014-06-02 18:09:33 +0900

    memo, util, version

--- lib/match_inchi/util.rb
+++ lib/match_inchi/util.rb
@@ -22,7 +22,29 @@ module MatchInChI
 
 	def match_loose_t(t1, t2)
 		# t field can include "."
-		a1, a2 = [t1, t2].map{ |t| t.split(/\,/) }
+		_t1, _t2 = t1[1..-1], t2[1..-1] # "t12-, 13-" => "12-, 13-"
+
+		h1, h2 = {}, {}
+	 	[_t1, _t2].zip([h1, h2]){ |t, h| 
+			t.split(/\,/).map{ |i| i =~ /(\d+)([+-?])/; h[$1] = $2 } 
+		}
+		[h1, h2].each{ |h| raise if h.has_key?(nil) }
+
+		h1.keys.each{ |p|
+			next unless h2[p] # pass if h2 don't have this position => e.g.) t1:"t12-,13?,14+", t2:"t12-,13-"
+			c1, c2 = h1[p], h2[p]  #=> +=?
+			if c1 == "?" or c2 == "?"
+				next
+			elsif c1 == c2
+				next
+			else
+				return nil
+			end
+		}
+		return 1
+
+=begin
+		#a1, a2 = [_t1, _t2].map{ |t| t.split(/\,/).map{ |i| i =~ /(\d+)([+-?])/; [$1, $2] } }
 		a1.zip(a2){ |e1, e2| # each element (6?/4+/...)
 			return nil if e1.size != e2.size
 			e1.split(//).zip(e2.split(//)){ |c1, c2| # each character
@@ -36,5 +58,6 @@ module MatchInChI
 			}
 		}
 		return 1
+=end
 	end
 end
